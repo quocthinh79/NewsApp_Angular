a096252f3745b669bcc9e200f690023c8e9df426
unlocker1205 <19130215@st.hcmuaf.edu.vn>
2022-07-29 17:30:46 +0700

    Hoàn thành tạo service call api cho cả hệ thống

--- ProjectNews/src/app/component/catalog-detail/catalog-detail.component.ts
+++ ProjectNews/src/app/component/catalog-detail/catalog-detail.component.ts
@@ -1,9 +1,10 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ApplicationRef, Component, OnDestroy, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import * as xml2js from "xml2js";
 import {RSSNews} from "../../model/NewsRSS";
-import {ActivatedRoute, Router} from "@angular/router";
+import {ActivatedRoute, Router, NavigationEnd } from "@angular/router";
 import {DataService} from "../../service/data.service";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-catalog-detail',
@@ -13,11 +14,20 @@ import {DataService} from "../../service/data.service";
 export class CatalogDetailComponent implements OnInit, OnDestroy {
   RssData: RSSNews;
 
-  constructor(private http: HttpClient, private route: ActivatedRoute, private router: Router, private feed: DataService) {
+  constructor(private http: HttpClient, private route: ActivatedRoute, private  service: DataService,private router: Router) {
     route.params.subscribe(val => {
       let id = this.route.snapshot.params.id;
-      this.RssData = feed.GetRssFeedData(id)
+      this.getRssFeedData(id);
     })
+  }
+
+  getRssFeedData(paramater: string) {
+    this.service.getData(paramater).subscribe(data => {
+      let parseString = xml2js.parseString;
+      parseString(data, (err, result: RSSNews) => {
+        this.RssData = result;
+      });
+    });
   }
 
   forRange(start: number, end: number) {
@@ -27,6 +37,11 @@ export class CatalogDetailComponent implements OnInit, OnDestroy {
     }
     return array;
   }
+
+  reloadCurrentRoute() {
+
+  }
+
 
   ngOnInit(): void {
   }
--- ProjectNews/src/app/service/data.service.ts
+++ ProjectNews/src/app/service/data.service.ts
@@ -1,28 +1,22 @@
-import { Injectable } from '@angular/core';
+import {Inject, Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {RSSNews} from "../model/NewsRSS";
 import * as xml2js from "xml2js";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-  RssData: RSSNews
+
   constructor(private http: HttpClient) {}
-  GetRssFeedData(paramater: string) {
+
+  getData(paramater: string) : Observable<any> {
     const requestOptions: Object = {
       observe: "body",
       responseType: "text"
     };
-    this.http
-      .get<any>("https://thethao247.vn/" + paramater + ".rss", requestOptions)
-      .subscribe(data => {
-        let parseString = xml2js.parseString;
-        parseString(data, (err, result: RSSNews) => {
-          this.RssData = result;
-        });
-
-      });
-    return this.RssData
+    return this.http
+      .get<Observable<any>>("https://thethao247.vn/" + paramater + ".rss", requestOptions);
   }
 }
