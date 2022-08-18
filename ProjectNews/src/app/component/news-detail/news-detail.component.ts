7c9c537fcb330a50d647cedc2590a787a7228d63
unlocker1205 <19130215@st.hcmuaf.edu.vn>
2022-08-18 20:15:38 +0700

    Đổ dữ liệu trang chi tiết

--- ProjectNews/src/app/component/news-detail/news-detail.component.ts
+++ ProjectNews/src/app/component/news-detail/news-detail.component.ts
@@ -3,6 +3,7 @@ import {DataService} from "../../service/data.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {RSSNews} from "../../model/NewsRSS";
 import {HttpClient} from "@angular/common/http";
+import * as xml2js from "xml2js";
 
 @Component({
   selector: 'app-news-detail',
@@ -10,14 +11,18 @@ import {HttpClient} from "@angular/common/http";
   styleUrls: ['./news-detail.component.scss']
 })
 export class NewsDetailComponent implements OnInit, OnDestroy {
+  dataHtml: any;
+
   constructor(private route: ActivatedRoute, private service: DataService) {
     route.params.subscribe( val => {
-      let id = this.route.snapshot.params.id;
-
+      let id = this.route.snapshot.params.detail;
+      this.getHtmlData(id);
     })
-
   }
 
+  getHtmlData(parameter: string) {
+    this.dataHtml = this.service.getDataHtml(parameter);
+  }
 
   ngOnInit(): void {
   }
