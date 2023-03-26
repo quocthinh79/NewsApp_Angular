d145a8500493cd7bf495ba0c36428b9599ce51f6
quocthinh79 <19130215@st.hcmuaf.edu.vn>
2023-03-26 20:22:04 +0700

    delete cors anywhere

--- ProjectNews/src/app/service/data.service.ts
+++ ProjectNews/src/app/service/data.service.ts
@@ -1,48 +1,51 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {RSSNews} from "../model/NewsRSS";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { RSSNews } from '../model/NewsRSS';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
-
   constructor(private http: HttpClient) {}
 
-  getDataRss(parameter: string) : Observable<any> {
+  getDataRss(parameter: string): Observable<any> {
     const requestOptions: Object = {
-      observe: "body",
-      responseType: "text",
+      observe: 'body',
+      responseType: 'text',
       headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-        "Access-Control-Allow-Origin" : "*",
-        "Access-Control-Allow-Credentials" : "true",
-        "Access-Control-Allow-Headers" : "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
-      }
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Allow-Headers':
+          'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers',
+      },
     };
-    const CORS_PROXY = "https://calm-spire-97456.herokuapp.com/"
-    return this.http
-      .get<Observable<RSSNews>>(CORS_PROXY + "https://thethao247.vn/" + parameter + ".rss", requestOptions);
+    return this.http.get<Observable<RSSNews>>(
+      'https://thethao247.vn/' + parameter + '.rss',
+      requestOptions
+    );
   }
 
   getDataHtml(parameter: string) {
     const requestOptions: Object = {
-      observe: "body",
-      responseType: "text",
+      observe: 'body',
+      responseType: 'text',
       headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-        "Access-Control-Allow-Origin" : "*",
-        "Access-Control-Allow-Credentials" : "true",
-        "Access-Control-Allow-Headers" : "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
-      }
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Allow-Headers':
+          'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers',
+      },
     };
-    const CORS_PROXY = "https://calm-spire-97456.herokuapp.com/"
-    return this.http
-      .get<Observable<any>>(CORS_PROXY + "https://thethao247.vn/" + parameter + ".html", requestOptions);
+    return this.http.get<Observable<any>>(
+      'https://thethao247.vn/' + parameter + '.html',
+      requestOptions
+    );
   }
 }
